f0dcbb41f1d4007235b832c6786e88b3520603ff
Mohd Faisal <mohd@Mohds-Mac-mini.local>
2024-01-23 23:23:11 +0530

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+rootProject.name = "Compose-Kotlin-Multiplatform-Exerices"
+include(":composeApp")
+
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
